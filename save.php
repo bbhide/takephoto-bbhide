86b52777634199a7ff8b2014e69969ab07b45cba
Bhavesh bhide <bhavesh.bhide@gmail.com>
2014-04-14 14:02:26 +0530

    Updated SWF file

--- save.php
+++ save.php
@@ -1,5 +1,5 @@
 <?php
 if(isset($GLOBALS["HTTP_RAW_POST_DATA"])){
 $jpg = $GLOBALS["HTTP_RAW_POST_DATA"];
-file_put_contents("image.jpg", $jpg);//Replace image.jpg with a dynamic name ;)
+file_put_contents("image.jpg", $jpg);
 }
